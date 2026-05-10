b0e41d80a955045d06e5f8fedc73aa0ee89781a0
Akintade Oluwaseun <akintadeseun816@gmail.com>
2026-05-10 14:31:45 +0100

    Initial Commit

---
+++ Shaders101/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shaders101
+//
+//  Created by BSTAR on 10/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shaders101/Shaders101App.swift
@@ -0,0 +1,17 @@
+//
+//  Shaders101App.swift
+//  Shaders101
+//
+//  Created by BSTAR on 10/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct Shaders101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
